11adfeb2e7412f73a1eb6abac464670939cff517
AlexPog70 <167794098+AlexPog70@users.noreply.github.com>
2024-04-23 00:39:13 +0200

    push

---
+++ L1_Informatique/ALGO_2/Algo2_CC2_2024/Exo2_ListesChaînées.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+struct arret {
+    std::string nom;
+    int duree;
+}
+struct maillon {
+    arret val;
+    maillon * suiv;
+}
+using ligne = maillon *;
+//1.
+void ajoutDebut(liste &l,arret A){liste p = new maillon;p->val.nom = A.nom;p->val.duree = A.duree;p->suiv=l;l=p;}
+void ajout_fin(liste &l,arret A){
+    if(l==nullptr) ajoutDebut(l,A);
+    else ajout_fin(l->suiv,A);}
+//2.
+bool recherche_arret(){}
+//3.
+void duree_trajet(){}
+//4.
+void arrets_communs(){}
+//5.
+bool bien_formee(){}
+//6.
+void insertion(){}
+//7.
+float duree_trajet2(){}
+
+int main(){
+    ligne tramway = nullptr;arret A;
+    return 0;}
