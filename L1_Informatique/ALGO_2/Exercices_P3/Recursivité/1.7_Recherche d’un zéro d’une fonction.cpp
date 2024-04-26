24e8544e90e85ac86513c42ca0ee365131be22f5
AlexPog70 <167794098+AlexPog70@users.noreply.github.com>
2024-04-26 17:47:53 +0000

    modified:   "L1_Informatique/ALGO_2/Exercices_P3/Recursivit\303\251/1.7_Recherche d\342\200\231un z\303\251ro d\342\200\231une fonction" 	modified:   "L1_Informatique/ALGO_2/Exercices_P3/Recursivit\303\251/1.7_Recherche d\342\200\231un z\303\251ro d\342\200\231une fonction.cpp"

--- L1_Informatique/ALGO_2/Exercices_P3/Recursivité/1.7_Recherche d’un zéro d’une fonction.cpp
+++ L1_Informatique/ALGO_2/Exercices_P3/Recursivité/1.7_Recherche d’un zéro d’une fonction.cpp
@@ -23,9 +23,26 @@ float min(float a,float b){
 
 float diff(float a,float b){return valeurAbs(max(a,b)) - (valeurAbs(min(a,b)));}
 
-void rech_zero(float &a, float &b,float &valMilieu){
-    if(f(a)*f(b)<0){
-        if(valeurAbs(diff(a,b))>0 && valeurAbs(diff(a,b))<0.1){
+bool prodNegatif(float x,float y){
+    if(f(x)*f(y)<0) return true;
+    else return false;}
+
+void rech_zero(float a, float b,float &valMilieu){
+    if(prodNegatif(a,b)){
+        valMilieu = min(a,b) + (diff(a,b)/2);
+        float M=0;//test
+        if(prodNegatif(valMilieu,min(a,b))) rech_zero(valMilieu,max(a,b),M);
+        else rech_zero(valMilieu,min(a,b),M);}}
+
+/*
+
+cout<<"Valeur de a : "<<min(a,b)<<" avec f(a) : "<<f(min(a,b))<<endl;
+        cout<<"Valeur de b : "<<max(a,b)<<" avec f(b) : "<<f(max(a,b))<<endl;
+        cout<<"Valeur du <<milieu>> : "<<valMilieu<<" avec f(milieu) : "<<f(valMilieu)<<endl;
+        cout<<"f(a)*f(b) : "<<f(a)*f(b)<<endl;cout<<"-------------------------------"<<endl;
+
+
+ if(valeurAbs(diff(a,b))>0 && valeurAbs(diff(a,b))<0.1){
             valMilieu= min(a,b) + (diff(a,b)/2);
             cout<<"Valeur de a : "<<min(a,b)<<" avec f(a) : "<<f(min(a,b))<<endl;
             cout<<"Valeur de b : "<<max(a,b)<<" avec f(b) : "<<f(max(a,b))<<endl;
@@ -34,28 +51,14 @@ void rech_zero(float &a, float &b,float &valMilieu){
             cout<<"f(a)*f(b) : "<<f(a)*f(b)<<endl;
             cout<<"---------------FIN--------------"<<endl;}
         else{
-            valMilieu = min(a,b) + (diff(a,b)/2);
-            cout<<"Valeur de a : "<<min(a,b)<<" avec f(a) : "<<f(min(a,b))<<endl;
-            cout<<"Valeur de b : "<<max(a,b)<<" avec f(b) : "<<f(max(a,b))<<endl;
-            cout<<"Valeur du <<milieu>> : "<<valMilieu<<" avec f(milieu) : "<<f(valMilieu)<<endl;
-            cout<<"f(a)*f(b) : "<<f(a)*f(b)<<endl;cout<<"-------------------------------"<<endl;
-            float M;//test
-            if(f(valMilieu)*f(a)>0){
-                rech_zero(valMilieu,b,M);}
-            else{
-                if(f(valMilieu)*f(b)>0) rech_zero(valMilieu,a,M);}}}
-    else{
-        if(f(a)*f(b)==0){
-            cout<<"Valeur de a : "<<min(a,b)<<endl;
-            cout<<"Valeur de b : "<<max(a,b)<<endl;}}}
+*/
 
 int main (){
-    float a,b;a=0;b=0;
+    float a,b,M;a=0;b=0;M=0;
     do{
         while(f(a)>0){a++;}
         b=a+1;while(f(b)<0){b++;}}
     while(f(a)*f(b)>0);
-    float M = min(a,b) + (diff(a,b)/2);
     rech_zero(a,b,M);
     return 0;}
 /*
