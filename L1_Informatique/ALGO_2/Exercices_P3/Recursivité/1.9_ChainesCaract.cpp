d062d27ada185a6338f22dbddd3ac07751d7a895
AlexPog70 <167794098+AlexPog70@users.noreply.github.com>
2024-05-09 15:46:02 +0000

    new file:   "L1_Informatique/ALGO_2/Exercices_P3/Recursivit\303\251/1.6_Nombres premiers.cpp" 	modified:   "L1_Informatique/ALGO_2/Exercices_P3/Recursivit\303\251/1.9_ChainesCaract.cpp" 	modified:   "L1_Informatique/ALGO_2/Exercices_P3/Recursivit\303\251/test"

--- L1_Informatique/ALGO_2/Exercices_P3/Recursivité/1.9_ChainesCaract.cpp
+++ L1_Informatique/ALGO_2/Exercices_P3/Recursivité/1.9_ChainesCaract.cpp
@@ -3,16 +3,23 @@ using namespace std;
 //1.
 string Extraire(string ch,int ind,int l){
     if(ind>-1 && ind<=ch.length()){
-        if(l==0) return "";
-        else{
-            if(l>0){
-                string s = Extraire(ch,ind+1,l-1);s=ch[ind]+s;
-                return s;}}
-    }
-}
+        if(l>0){
+            string s = Extraire(ch,ind+1,l-1);
+            s=ch[ind]+s;
+            return s;}
+        else return "";}
+    else return "";}
+//2.
+bool palindrome(string ch){
+    if(ch =="" or ch.length()==1) return true;
+    else{
+        if(ch[0]==ch[ch.length()-1]) return palindrome(Extraire(ch,1,ch.length()-2));
+        else return false;}}
+//3.
+int nbreOc(string ch,char c){
+    if(ch=="") return 0;
+    else{
+        if(ch[0]==c) return 1+nbreOc(Extraire(ch,1,ch.length()-1),c);
+        else return nbreOc(Extraire(ch,1,ch.length()-1),c);}}
 
-int main(){
-    string ch;cout<<"Chaine : ";cin>>ch;
-    cout<<Extraire(ch,3,4)<<endl;
-    return 0;
-}
+int main(){return 0;}
