23358ba3538d54db8f75ccf8621a5ccae24f6dc9
AlexPog70 <167794098+AlexPog70@users.noreply.github.com>
2024-06-10 16:32:35 +0000

    modified:   L1_Informatique/Algorithmique/ALGO_1/Exercices_P1/4 Tableaux/4.5_Histogrammes.cpp 	modified:   L1_Informatique/Algorithmique/ALGO_1/Exercices_P1/4 Tableaux/test

--- L1_Informatique/Algorithmique/ALGO_1/Exercices_P1/4 Tableaux/4.5_Histogrammes.cpp
+++ L1_Informatique/Algorithmique/ALGO_1/Exercices_P1/4 Tableaux/4.5_Histogrammes.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <array>
+#include <string>
+
 using namespace std;
 
 int main(){
-    const int N=100;array<int,N> Nb;int i,n;n=0;
-    for(int j = 0; j < N; j++) Nb[j]=0;
+    const int N=21;array<int,N> Nb;int i,cpt;cpt=0;// i =note et cpt nbre de notes
+    for(int j = 0; j < N; j++) Nb[j]=0;//Initialisation
+    cout<<"Après saisie des notes, entrez une valeur hors bornes (0- ou 20+) !"<<endl;
     cout<<"Saisir une suite de notes entre 0 et 20 : ";cin>>i;
-    while(i>=0 && i<=20 && i<=100){n++;Nb[i]=Nb[i]+1;cin>>i;}
-    //Affichage sous histogramme
-    for (int j = 0; j < i; j++){cout<<tab[j]<<' ';}cout<<endl;
-    return 0;}
+    while(i>=0 && i<=20){cpt+=1;Nb[i]=Nb[i]+1;cin>>i;}
+    /*Affichage sous histogramme horizontal
+    for (int i = 0; i <= N; i++){
+        cout<<i<<" : ";for(int k=0;k<Nb[i];k++){cout<<'*';}
+        cout<<endl;}*/
+    //Affichage sous histogramme vertical
+    // Etape1:Recherche du nombre-indice ayant le +grd Nbre d'occurences dans Nb.
+     int indmax = 0;
+     for(int i = 1; i < N; i++) if(Nb[i]>Nb[indmax]) indmax=i;//
+     //Etape2:Construction puis affichage des étoiles.
+     
+     return 0;}
