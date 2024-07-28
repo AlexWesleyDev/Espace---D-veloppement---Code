bd95c2789260c0ec521e74e767e23da5e038f877
AlexWesleyDev <135975112+AlexWesleyDev@users.noreply.github.com>
2024-07-28 12:38:01 +0000

    new file:   L1_Informatique/Algorithmique/ALGO_2/Exercices_P3/III-Pointeurs/3.1_EtudeProgrammes.cpp 	new file:   "L1_Informatique/Algorithmique/ALGO_2/Exercices_P3/III-Pointeurs/3.2_TableauxAllou\303\251sDynamique.cpp" 	new file:   L1_Informatique/Algorithmique/ALGO_2/Exercices_P3/III-Pointeurs/brouillon.cpp 	new file:   L1_Informatique/Algorithmique/ALGO_2/Exercices_P3/III-Pointeurs/fichier.txt 	new file:   L1_Informatique/Algorithmique/ALGO_2/Exercices_P3/III-Pointeurs/test 	modified:   L1_Informatique/TestResultatNote.cpp 	modified:   L1_Informatique/test

--- L1_Informatique/TestResultatNote.cpp
+++ L1_Informatique/TestResultatNote.cpp
@@ -4,13 +4,43 @@
 using namespace std;
 
 int main(){
-    float Note1,Note2,Note3,Note4,Resultat;float Bd,Algo2,Aritm;
-    cout<<"Note en BD : ";cin>>Bd;cout<<"Note en arithmétique : ";cin>>Aritm;cout<<"Note en Algo2 : ";cin>>Algo2;
-    Note1 = (11*7+Algo2*8+4.84*6+14)/22;Note2=(16*4+8.5*2+13*3+Bd*4)/13;Note3=(11*5+7*5+Aritm*3)/13;Note4=(11+2*11.5+10.3+2+11.5*5)/10;
-    Resultat=(Note1+Note2+Note3+Note4)/4;
-    cout<<"Bloc F.I et ALGO : "<<Note1<<endl;
-    cout<<"Bloc Développement : "<<Note2<<endl;
-    cout<<"Bloc Mathématique : "<<Note3<<endl;
-    cout<<"Bloc Transversaux : "<<Note4<<endl;
+    float Moyenne1,Moyenne2,Moyenne3,Moyenne4,Resultat;
+    float Algo1,Algo2,FondInfo,BaseInfo,DevWeb,Linux,Python,BaseDonnees,Analyse,Algebre,Aritm;
+    float Anglais,EEO,PPPE,CultNum,Concretisation;
+    // BLOC MATHEMATIQUE
+    cout<<"// BLOC MATHEMATIQUE "<<endl;
+    cout<<"Note en Analyse : ";cin>>Analyse;
+    cout<<"Note en Algèbre : ";cin>>Algebre;
+    cout<<"Note en Arithmétique : ";cin>>Aritm;
+    // BLOC FONDEMENTS ET ALGORITHMIQUE
+    cout<<"// BLOC FONDEMENTS ET ALGORITHMIQUE "<<endl;
+    cout<<"Note en Base d'info : ";cin>>BaseInfo;
+    cout<<"Note en Algo1 : ";cin>>Algo1;
+    cout<<"Note en Algo2 : ";cin>>Algo2;
+    cout<<"Note en Fond.Info : ";cin>>FondInfo;
+    // BLOC DEVELOPPEMENT
+    cout<<"// BLOC DEVELOPPEMENT "<<endl;
+    cout<<"Note en Linux : ";cin>>Linux;
+    cout<<"Note en Développement WEB : ";cin>>DevWeb;
+    cout<<"Note en Développement Python : ";cin>>Python;
+    cout<<"Note en Base de données : ";cin>>BaseDonnees;
+    // BLOC TRANSVERSAUX
+    cout<<"// BLOC TRANSVERSAUX "<<endl;
+    cout<<"Note en Culture numérique : ";cin>>CultNum;
+    cout<<"Note en Expression Ecrite Orale : ";cin>>EEO;
+    cout<<"Note en Anglais : ";cin>>Anglais;
+    cout<<"Note en 3PE : ";cin>>PPPE;
+    cout<<"Note en Concrétisation : ";cin>>Concretisation;
+    // CALCUL
+    cout<<"// CALCUL "<<endl;
+    Moyenne1 = (Algo1*7 + Algo2*8 + FondInfo*6 + BaseInfo*1)/22;
+    Moyenne2 = (DevWeb*4 + Linux*2 + Python*3 + BaseDonnees*4)/13;
+    Moyenne3=(Analyse*5 + Algebre*5 + Aritm*3)/13;
+    Moyenne4=(Anglais*1 + EEO*2 + PPPE*1 + CultNum*1 + Concretisation*5)/10;
+    Resultat=(Moyenne1 + Moyenne2 + Moyenne3 + Moyenne4)/4;// MOYENNE DES 4 BLOCS
+    cout<<"Moyenne Bloc F.I et ALGO : "<< Moyenne1 <<endl;
+    cout<<"Moyenne Bloc Développement : "<< Moyenne2 <<endl;
+    cout<<"Moyenne Bloc Mathématique : "<< Moyenne3 <<endl;
+    cout<<"Moyenne Bloc Transversaux : "<< Moyenne4 <<endl;
     cout<<"Le résultat pour l'année serait de "<<Resultat<<'.'<<endl;
     return 0;}
