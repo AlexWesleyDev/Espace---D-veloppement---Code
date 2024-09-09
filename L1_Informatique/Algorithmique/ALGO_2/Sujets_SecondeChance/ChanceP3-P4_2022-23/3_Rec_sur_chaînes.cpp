b1a54e6b4f5fc5bdfd064e1156607f7bfb054a54
AlexPog70 <167794098+AlexPog70@users.noreply.github.com>
2024-09-09 12:16:51 +0000

    modified:   "L1_Informatique/Algorithmique/ALGO_2/Sujets_SecondeChance/ChanceP3-P4_2022-23/3_Rec_sur_cha\303\256nes.cpp"

--- L1_Informatique/Algorithmique/ALGO_2/Sujets_SecondeChance/ChanceP3-P4_2022-23/3_Rec_sur_chaînes.cpp
+++ L1_Informatique/Algorithmique/ALGO_2/Sujets_SecondeChance/ChanceP3-P4_2022-23/3_Rec_sur_chaînes.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <string>
 #include <array>
+using namespace std;
 
-////////// A REFAIRE /////////////////
-
-std::string retourner(std::string &s,int k){
-    if(k==0) return s[k];
-    else{
-        if(k>0) return s[k-1] + retourner(s,k--);}}
+string retourner(string s,unsigned int k){
+    if(k==0) return "";
+    return s[k-1] + retourner(s,k-1);}
 
 int main (){
-    std::string s;int k;
+    std::string s;unsigned int k;
     std::cout << "Saisir une chaine : ";std::cin>>s;
     std::cout << "Saisir sa taille : ";std::cin >>k;
-    std::cout<<retourner(s,k)<<std::endl;
-    return 0;
-}
+    //test de taille
+    if(k>s.size()) cout<< "Erreur : La taille saisie dépasse la longueur véritable de la chaîne."<<endl;
+    else cout<<"Chaîne inversée : "<<retourner(s, k)<<endl;
+    return 0;}
