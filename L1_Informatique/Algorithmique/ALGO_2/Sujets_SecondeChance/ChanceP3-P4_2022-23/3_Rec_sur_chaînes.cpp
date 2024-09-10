cb18143d2ff7ef44e109e6c395714d1e25a7c662
AlexPog70 <167794098+AlexPog70@users.noreply.github.com>
2024-09-10 11:31:16 +0000

    modified:   "L1_Informatique/Algorithmique/ALGO_2/Sujets_SecondeChance/ChanceP3-P4_2022-23/3_Rec_sur_cha\303\256nes.cpp"

--- L1_Informatique/Algorithmique/ALGO_2/Sujets_SecondeChance/ChanceP3-P4_2022-23/3_Rec_sur_chaînes.cpp
+++ L1_Informatique/Algorithmique/ALGO_2/Sujets_SecondeChance/ChanceP3-P4_2022-23/3_Rec_sur_chaînes.cpp
@@ -12,6 +12,7 @@ int main (){
     std::cout << "Saisir une chaine : ";std::cin>>s;
     std::cout << "Saisir sa taille : ";std::cin >>k;
     //test de taille
-    if(k>s.size()) cout<< "Erreur : La taille saisie dépasse la longueur véritable de la chaîne."<<endl;
+    if(k>s.size() or k<s.size()){cout<< "Erreur : La taille saisie ne correspond pas à la longueur véritable de la chaîne."<<endl;
+    cout<<"Chaîne inversée selon la vraie taille ("<<s.size()<<") : "<< retourner(s, s.size())<<endl;}
     else cout<<"Chaîne inversée : "<<retourner(s, k)<<endl;
     return 0;}
