6edc325474b7f6f8c3742e40323dbaf7ab2e5a5f
AlexPog70 <167794098+AlexPog70@users.noreply.github.com>
2024-09-29 19:30:13 +0000

    modified:   BTS SIO 1/PROGRAMMATION/TC3/obj/Debug/net8.0/TC3.AssemblyInfo.cs 	modified:   BTS SIO 1/PROGRAMMATION/TC3/obj/Debug/net8.0/TC3.AssemblyInfoInputs.cache 	modified:   BTS SIO 1/PROGRAMMATION/TC4/bin/Debug/net8.0/TC4.dll 	modified:   BTS SIO 1/PROGRAMMATION/TC4/bin/Debug/net8.0/TC4.pdb 	modified:   BTS SIO 1/PROGRAMMATION/TC4/obj/Debug/net8.0/TC4.AssemblyInfo.cs 	modified:   BTS SIO 1/PROGRAMMATION/TC4/obj/Debug/net8.0/TC4.AssemblyInfoInputs.cache 	modified:   BTS SIO 1/PROGRAMMATION/TC4/obj/Debug/net8.0/TC4.dll 	modified:   BTS SIO 1/PROGRAMMATION/TC4/obj/Debug/net8.0/TC4.pdb 	modified:   BTS SIO 1/PROGRAMMATION/TC4/obj/Debug/net8.0/TC4.sourcelink.json 	modified:   BTS SIO 1/PROGRAMMATION/TC4/obj/Debug/net8.0/ref/TC4.dll 	modified:   BTS SIO 1/PROGRAMMATION/TC4/obj/Debug/net8.0/refint/TC4.dll

--- BTS SIO 1/PROGRAMMATION/TC3/obj/Debug/net8.0/TC3.AssemblyInfo.cs
+++ BTS SIO 1/PROGRAMMATION/TC3/obj/Debug/net8.0/TC3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TC3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+857706546603ef9c705a3e44bfac5eb32896cf1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c86ccf1b1384783bb573090df963b743c22151")]
 [assembly: System.Reflection.AssemblyProductAttribute("TC3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TC3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
