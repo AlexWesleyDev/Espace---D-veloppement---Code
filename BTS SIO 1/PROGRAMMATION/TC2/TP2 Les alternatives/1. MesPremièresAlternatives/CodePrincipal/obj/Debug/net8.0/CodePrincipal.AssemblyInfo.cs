013bc0858d63f4572337bbeee2f08c1021dfc229
AlexPog70 <167794098+AlexPog70@users.noreply.github.com>
2024-10-01 15:46:31 +0000

    modified:   BTS SIO 1/Developpement Web/tips.txt 	modified:   "BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/1. MesPremi\303\250resAlternatives/CodePrincipal/obj/Debug/net8.0/CodePrincipal.AssemblyInfo.cs" 	modified:   "BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/1. MesPremi\303\250resAlternatives/CodePrincipal/obj/Debug/net8.0/CodePrincipal.AssemblyInfoInputs.cache" 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/2. AnalyserModifierProgramme/Modification1/obj/Debug/net8.0/Modification1.AssemblyInfo.cs 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/2. AnalyserModifierProgramme/Modification1/obj/Debug/net8.0/Modification1.AssemblyInfoInputs.cache 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/2. AnalyserModifierProgramme/Modification2/obj/Debug/net8.0/Modification2.AssemblyInfo.cs 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/2. AnalyserModifierProgramme/Modification2/obj/Debug/net8.0/Modification2.AssemblyInfoInputs.cache 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/2. AnalyserModifierProgramme/Modification3/obj/Debug/net8.0/Modification3.AssemblyInfo.cs 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/2. AnalyserModifierProgramme/Modification3/obj/Debug/net8.0/Modification3.AssemblyInfoInputs.cache 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/2. AnalyserModifierProgramme/Modification4/obj/Debug/net8.0/Modification4.AssemblyInfo.cs 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/2. AnalyserModifierProgramme/Modification4/obj/Debug/net8.0/Modification4.AssemblyInfoInputs.cache

--- BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/1. MesPremièresAlternatives/CodePrincipal/obj/Debug/net8.0/CodePrincipal.AssemblyInfo.cs
+++ BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/1. MesPremièresAlternatives/CodePrincipal/obj/Debug/net8.0/CodePrincipal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePrincipal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109c037e5e6e541629af1e18e9edfe0167043fff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8da0e21c310bbf8dea5e780aa25fe60c85ac0c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePrincipal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePrincipal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
