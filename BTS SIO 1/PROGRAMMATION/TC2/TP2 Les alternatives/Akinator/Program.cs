ea2d344da45f0d5855342b36e6e6899db25e63d2
AlexPog70 <167794098+AlexPog70@users.noreply.github.com>
2024-10-07 11:27:23 +0000

    modified:   BTS SIO 1/Developpement Web/BLOG/style.css 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/Akinator.csproj 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/Program.cs 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/bin/Debug/net8.0/Akinator 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/bin/Debug/net8.0/Akinator.deps.json 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/bin/Debug/net8.0/Akinator.dll 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/bin/Debug/net8.0/Akinator.pdb 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/bin/Debug/net8.0/Akinator.runtimeconfig.json 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Akinator.csproj.nuget.dgspec.json 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Akinator.csproj.nuget.g.props 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Akinator.csproj.nuget.g.targets 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/Akinator.AssemblyInfo.cs 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/Akinator.AssemblyInfoInputs.cache 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/Akinator.GeneratedMSBuildEditorConfig.editorconfig 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/Akinator.GlobalUsings.g.cs 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/Akinator.assets.cache 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/Akinator.csproj.CoreCompileInputs.cache 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/Akinator.csproj.FileListAbsolute.txt 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/Akinator.dll 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/Akinator.genruntimeconfig.cache 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/Akinator.pdb 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/Akinator.sourcelink.json 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/apphost 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/ref/Akinator.dll 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/Debug/net8.0/refint/Akinator.dll 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/project.assets.json 	new file:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/obj/project.nuget.cache 	new file:   Espace-DevCod.sln

---
+++ BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/Akinator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program5{
+    static void Akinator0(){
+        Console.WriteLine("Pensez à un personnage parmi ceux ci : Wonder Woman, Batman, Groot, Hulk, Lara Croft.");
+        Console.Write("Est-ce que c'est un humain ? (oui/non) : ");string reponse = Console.ReadLine().ToLower();
+        if (reponse == "oui"){
+            Console.Write("Est-ce que c'est un homme ? (oui/non) : ");string rep1 = Console.ReadLine().ToLower();
+            if(rep1 == "oui"){Console.WriteLine("C'est Batman !");}
+            else{
+                Console.Write("Est-ce que le personnage a deux pistolets ? (oui/non) : ");
+                string rep2 = Console.ReadLine().ToLower();
+                if(rep2=="oui"){Console.WriteLine("C'est Lara Croft !");}
+                else{Console.WriteLine("C'est Wonder Woman !");}}}
+        else{
+            if (reponse == "non"){
+                Console.Write("Est-ce que le personnage est vert ? (oui/non) : ");string reponse1 = Console.ReadLine().ToLower();
+                if(reponse1=="oui"){Console.WriteLine("C'est Hulk !");}
+                else{Console.WriteLine("C'est Je S'appelle Groot !");}}
+            else{Console.WriteLine("Saisie incorrecte");}}}
+    
+    static void Main(string[] args){
+    Akinator0();
+    Console.ReadKey(true);}
+}
