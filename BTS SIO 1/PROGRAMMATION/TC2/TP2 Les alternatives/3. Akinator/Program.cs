57afdd64d77802f6334d14d57a8848776c3c619c
AlexWesleyDev <135975112+AlexWesleyDev@users.noreply.github.com>
2024-10-08 17:54:46 +0000

    modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/Program.cs 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/bin/Debug/net8.0/3. Akinator.dll 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/bin/Debug/net8.0/3. Akinator.pdb 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/obj/3. Akinator.csproj.nuget.dgspec.json 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/obj/3. Akinator.csproj.nuget.g.props 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/obj/Debug/net8.0/3. Akinator.AssemblyInfo.cs 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/obj/Debug/net8.0/3. Akinator.AssemblyInfoInputs.cache 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/obj/Debug/net8.0/3. Akinator.dll 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/obj/Debug/net8.0/3. Akinator.pdb 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/obj/Debug/net8.0/3. Akinator.sourcelink.json 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/obj/Debug/net8.0/ref/3. Akinator.dll 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/obj/Debug/net8.0/refint/3. Akinator.dll 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/obj/project.assets.json 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/obj/project.nuget.cache

--- BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/Program.cs
+++ BTS SIO 1/PROGRAMMATION/TC2/TP2 Les alternatives/3. Akinator/Program.cs
@@ -1,27 +1,25 @@
-﻿using System;
-
-namespace TP3
-{
-    internal class Program{
-        /*/1 - Ce programme affiche de manière itérative dans la boucle FOR tous les nombbres négatifs par pas de 1 de 0 à -30 non inclus.
-        static void Main(string[] args){
-            int cpt = 0;
-            for (cpt = 0; cpt > -30; cpt--){
-                if (cpt != 0 && cpt % 10 == 0){Console.WriteLine("CHANGEMENT DE DIZAINE");}
-                Console.WriteLine("le compteur est passé à " + cpt);}}*/
-        //2 - Afficher les nombres impairs jusqu’à 100 puis les afficher 10 par 10.
-        /*static void Main(string[] args){
-            int cpt = 0; int compteur=0;
-            for (cpt = 0; cpt <= 100; cpt++){
-                if (cpt%2!=0){
-                    Console.WriteLine("le compteur est passé à " + cpt);compteur++;
-                    if(compteur%10==0){ Console.Write("\n");}}}}*/
-        //3
-        static void Main(string[] args){
-            for (int cpt = 0; cpt < 10; cpt++){
-                for (int i = 1; i <=30; i++){
-                    if(i < 11){Console.ForegroundColor = ConsoleColor.Blue;Console.Write("*");}
-                    if (i > 10 && i < 21){Console.ForegroundColor = ConsoleColor.White;Console.Write("*");}
-                    if (i > 20){Console.ForegroundColor = ConsoleColor.Red;Console.Write("*");}}
-                Console.WriteLine("");}
-            Console.ReadKey();}}}
+﻿// A MODIFIER IMPERATIVEMENT
+using System;
+class Program5{
+    static void Akinator0(){
+        Console.WriteLine("Pensez à un personnage parmi ceux ci : Wonder Woman, Batman, Groot, Hulk, Lara Croft.");
+        Console.Write("Est-ce que c'est un humain ? (oui/non) : ");string reponse = Console.ReadLine().ToLower();
+        if (reponse == "oui"){
+            Console.Write("Est-ce que c'est un homme ? (oui/non) : ");string rep1 = Console.ReadLine().ToLower();
+            if(rep1 == "oui"){Console.WriteLine("C'est Batman !");}
+            else{
+                Console.Write("Est-ce que le personnage a deux pistolets ? (oui/non) : ");
+                string rep2 = Console.ReadLine().ToLower();
+                if(rep2=="oui"){Console.WriteLine("C'est Lara Croft !");}
+                else{Console.WriteLine("C'est Wonder Woman !");}}}
+        else{
+            if (reponse == "non"){
+                Console.Write("Est-ce que le personnage est vert ? (oui/non) : ");string reponse1 = Console.ReadLine().ToLower();
+                if(reponse1=="oui"){Console.WriteLine("C'est Hulk !");}
+                else{Console.WriteLine("C'est Je S'appelle Groot !");}}
+            else{Console.WriteLine("Saisie incorrecte");}}}
+    
+    static void Main(string[] args){
+    Akinator0();
+    Console.ReadKey(true);}
+}
