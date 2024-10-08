0bf575d85d36ea56ac2e6c1213c8367b2df236a5
AlexWesleyDev <135975112+AlexWesleyDev@users.noreply.github.com>
2024-10-08 17:51:03 +0000

    deleted:    BTS SIO 1/BOUCLE TANT QUE/Ex2.py 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/Program.cs 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/bin/Debug/net8.0/Exercice1.dll 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/bin/Debug/net8.0/Exercice1.pdb 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/obj/Debug/net8.0/Exercice1.AssemblyInfo.cs 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/obj/Debug/net8.0/Exercice1.AssemblyInfoInputs.cache 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/obj/Debug/net8.0/Exercice1.dll 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/obj/Debug/net8.0/Exercice1.pdb 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/obj/Debug/net8.0/Exercice1.sourcelink.json 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/obj/Debug/net8.0/ref/Exercice1.dll 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/obj/Debug/net8.0/refint/Exercice1.dll 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/obj/Exercice1.csproj.nuget.dgspec.json 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/obj/Exercice1.csproj.nuget.g.props 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/obj/project.assets.json 	modified:   BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/obj/project.nuget.cache

--- BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/Program.cs
+++ BTS SIO 1/PROGRAMMATION/TC2/TP3 Boucle Pour/Exercice1/Program.cs
@@ -1,32 +1,27 @@
 ﻿using System;
-class Program{
-    /*/1 - Ce programme affiche de manière itérative dans la boucle FOR tous les nombbres négatifs par pas de 1 de 0 à -30 non inclus.
-    static void Main(string[] args){
-        int cpt = 0;
-        for (cpt = 0; cpt > -30; cpt--){
-            if (cpt != 0 && cpt % 10 == 0){Console.WriteLine("CHANGEMENT DE DIZAINE");}
-            Console.WriteLine("le compteur est passé à " + cpt);}}*/
-    //2 - Afficher les nombres impairs jusqu’à 100 puis les afficher 10 par 10.
-    /*static void Main(string[] args){
-        int cpt = 0; int compteur=0;
-        for (cpt = 0; cpt <= 100; cpt++){
-            if (cpt%2!=0){
-                Console.WriteLine("le compteur est passé à " + cpt);compteur++;
-                if(compteur%10==0){ Console.Write("\n");}}}}*/
-    //3
-    static void Main(string[] args){
-        for (int cpt = 0; cpt <10; cpt++){
-            for(int i=0;i<30;i++){
-                if(i<11){
-                Console.Foreground = ConsoleColor.Blue;
-                Console.Write("*");}
-                if(i>10 && i<21){
-                Console.Foreground = ConsoleColor.white;
-                Console.Write("*");}
-                if(i>20){
-                    Console.Foreground=ConsoleColor.red;
-                    Console.Write("*");}}}}
-    Console.Readkey();
-}
 
-/* */
+namespace TP3
+{
+    internal class Program{
+        /*/1 - Ce programme affiche de manière itérative dans la boucle FOR tous les nombbres négatifs par pas de 1 de 0 à -30 non inclus.
+        static void Main(string[] args){
+            int cpt = 0;
+            for (cpt = 0; cpt > -30; cpt--){
+                if (cpt != 0 && cpt % 10 == 0){Console.WriteLine("CHANGEMENT DE DIZAINE");}
+                Console.WriteLine("le compteur est passé à " + cpt);}}*/
+        //2 - Afficher les nombres impairs jusqu’à 100 puis les afficher 10 par 10.
+        /*static void Main(string[] args){
+            int cpt = 0; int compteur=0;
+            for (cpt = 0; cpt <= 100; cpt++){
+                if (cpt%2!=0){
+                    Console.WriteLine("le compteur est passé à " + cpt);compteur++;
+                    if(compteur%10==0){ Console.Write("\n");}}}}*/
+        //3
+        static void Main(string[] args){
+            for (int cpt = 0; cpt < 10; cpt++){
+                for (int i = 1; i <=30; i++){
+                    if(i < 11){Console.ForegroundColor = ConsoleColor.Blue;Console.Write("*");}
+                    if (i > 10 && i < 21){Console.ForegroundColor = ConsoleColor.White;Console.Write("*");}
+                    if (i > 20){Console.ForegroundColor = ConsoleColor.Red;Console.Write("*");}}
+                Console.WriteLine("");}
+            Console.ReadKey();}}}
