3d8f3c7831461524a5e220ddd7e29d38a0b45cb8
AlexWesleyDev <135975112+AlexWesleyDev@users.noreply.github.com>
2024-11-18 15:02:42 +0000

    modified:   BTS SIO 1/PROGRAMMATION/TC4/TP3.2_Boucle Tant Que/Exercice2/Version_1/Program.cs 	modified:   BTS SIO 1/PROGRAMMATION/TC4/TP3.2_Boucle Tant Que/Exercice2/Version_2/Program.cs

--- BTS SIO 1/PROGRAMMATION/TC4/TP3.2_Boucle Tant Que/Exercice2/Version_1/Program.cs
+++ BTS SIO 1/PROGRAMMATION/TC4/TP3.2_Boucle Tant Que/Exercice2/Version_1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace TP3_Version1{
+    internal class Program{
+        static double piece;static string inputPiece;
+        const double PrixCafé = 0.50;
+
+        static void Main(string[] args){
+            Console.WriteLine("Quelle pièce insérez-vous ?");
+            Console.WriteLine("- 1 pour 1 euro\n- 2 pour 2 euros\n- 10 pour 0,10 euro\n- 20 pour 0,20 euro\n- 50 pour 0,50 euro");
+            inputPiece=Console.ReadLine();
+            while(string.IsNullOrEmpty(inputPiece) || !double.TryParse(inputPiece,out piece) || (piece!=1 && piece!=2 && piece!=10
+            && piece!=20 && piece!=50))
+            {
+                Console.WriteLine("Erreur de saisie, recommencez : ");
+                inputPiece=Console.ReadLine();
+            }
+            if(piece==1 || piece==2){Console.WriteLine("Vous avez inséré "+piece+" euro(s)");}
+            else{Console.WriteLine("Vous avez inséré "+piece+" centimes d'euro");}
+            // Gestion de l'état du montant inséré selon le prix du café.
+            if(piece==10 || piece==20 || piece==50){piece=piece*0.01;}
+            //
+            if(piece>=PrixCafé){
+                    if(piece>PrixCafé){Console.WriteLine("Vous avez trop versé, nous vous devons "+(piece-PrixCafé)+". RECUPERER VOS PIECES !");}
+                    Console.WriteLine("******CAFE en cours de PREPARATION******");
+            }
+            else{if(piece<PrixCafé){Console.WriteLine("Montant insuffisant, reste à payer "+(PrixCafé-piece));}}
+        }
+    }
+}
--- BTS SIO 1/PROGRAMMATION/TC4/TP3.2_Boucle Tant Que/Exercice2/Version_2/Program.cs
+++ BTS SIO 1/PROGRAMMATION/TC4/TP3.2_Boucle Tant Que/Exercice2/Version_2/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace TP3_Version2{
+    internal class Program{
+        static double piece,total=0;static string inputPiece;
+        const double PrixCafé = 0.50;
+
+        static void ChoixBoisson(){
+            int boisson;string inputBoisson;
+            Console.WriteLine("Choisissez votre boisson chaud :");
+            Console.WriteLine("1. Café nature avec sucre\n2. Chocolat chaud\n3. Thé\n4. Cappuccino\n5. Café Latte");
+            inputBoisson=Console.ReadLine();
+            while(string.IsNullOrEmpty(inputBoisson)||!int.TryParse(inputBoisson,out boisson)||boisson>5||boisson<1)
+            {
+                Console.Write("Erreur, veuillez bien choisir le numéro de votre boisson : ");inputBoisson=Console.ReadLine();
+            }
+            switch (boisson)
+            {
+                case 1:
+                Console.WriteLine("Café nature avec sucre sélectionné.");
+                break;
+                case 2:
+                Console.WriteLine("Chocolat chaud sélectionné.");
+                break;
+                case 3:
+                Console.WriteLine("Thé sélectionné.");
+                break;
+                case 4:
+                Console.WriteLine("Cappuccino sélectionné.");
+                break;
+                case 5:
+                Console.WriteLine("Café latte sélectionné.");
+                break;                
+                default:
+                Console.WriteLine("Merci de votre choix.");
+                break;
+            }
+        }
+
+        static void Main(string[] args){
+            do{
+                ChoixBoisson();
+                Console.WriteLine("Quelle pièce insérez-vous ?");
+                Console.WriteLine("- 1 pour 1 euro\n- 2 pour 2 euros\n- 10 pour 0,10 euro\n- 20 pour 0,20 euro\n- 50 pour 0,50 euro");
+                // Gestion de l'état du montant total inséré par rapport au prix du café.
+                do{
+                    inputPiece=Console.ReadLine();
+                    while(string.IsNullOrEmpty(inputPiece) || !double.TryParse(inputPiece,out piece) || (piece!=1 && piece!=2 && piece!=10
+                    && piece!=20 && piece!=50))
+                    {
+                        Console.Write("Erreur de saisie, recommencez : ");
+                        inputPiece=Console.ReadLine();
+                    }
+                    if(piece==10 || piece==20 || piece==50){piece=piece*0.01;}
+                    total+=piece;
+                    if(total<PrixCafé){Console.Write("Montant : "+total+" euro(s)"+". Introduire des pièces : ");}
+                }while(total<PrixCafé);
+
+                Console.WriteLine("Total = "+total);// Test à retirer !!!!
+
+                if(total>=1){Console.WriteLine("Vous avez inséré "+total+" euro(s)");}
+                else{Console.WriteLine("Vous avez inséré "+total+" centimes d'euro");}            
+                //
+                if(total>=PrixCafé){
+                        if(total>PrixCafé){Console.WriteLine("Vous avez trop versé, nous vous devons "+(total-PrixCafé)+". RECUPERER VOS PIECES !");}
+                        Console.WriteLine("******CAFE en cours de PREPARATION******");//Console.WriteLine("******CAFE prêt.Terminé !******");
+                }
+                Console.Write("Souhaitez-vous quitter ? (O/N):\n");inputPiece=Console.ReadLine();
+            }while(!string.IsNullOrEmpty(inputPiece) && inputPiece=="N");//On arrête la préparation si l'utilisateur décide de quitter.
+        }
+    }
+}
+
+//PENSEZ A GERER L'ENSEMBLE DU PROGRAMME COMPLET, AJOUTER DES COULEURS SI POSSIBLE ET TERMINER LE TP POUR DEMAIN.
